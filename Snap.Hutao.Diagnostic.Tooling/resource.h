44985d82958ba6c51b498cb1cdaba1e71f2165e9
qhy040404 <qhy040404@163.com>
2024-02-18 21:13:04 +0800

    update

--- Snap.Hutao.Diagnostic.Tooling/resource.h
+++ Snap.Hutao.Diagnostic.Tooling/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ ���ɵİ����ļ���
-// �� Snap.Hutao.Diagnostic.Tooling.rc ʹ��
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Snap.Hutao.DiagTools.rc 使用
 //
 #define IDI_ICON1                       103
 
